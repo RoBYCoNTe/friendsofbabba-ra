2bf2e4ffc2b645bd081d3cb51c39fbf883fd775b
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2022-06-10 11:09:20 +0200

    Dev: added BlogPostComment example.

--- src/component/crud/useCustomComponents.js
+++ src/component/crud/useCustomComponents.js
@@ -1,13 +1,20 @@
-import { useMemo } from "react";
+import { useContext, useMemo } from "react";
+
+import { CrudContext } from "../../data/cakephp/crud/CrudContext";
 
 const { getResources } = require("ra-core");
 const { shallowEqual, useSelector } = require("react-redux");
 const { get } = require("lodash");
+
 const useCustomComponents = (resource) => {
+  const { components } = useContext(CrudContext);
   const resources = useSelector(getResources, shallowEqual);
   return useMemo(() => {
     const r = resources.find((r) => r.name === resource);
-    return get(r, "options.components");
-  }, [resources, resource]);
+    return {
+      ...get(r, "options.components"),
+      ...components,
+    };
+  }, [components, resources, resource]);
 };
 export default useCustomComponents;
--- src/component/form/ReferenceToolbar.js
+++ src/component/form/ReferenceToolbar.js
@@ -47,7 +47,7 @@ const ReferenceToolbar = ({
       <SaveButton
         redirect={backUrl}
         disabled={!canSave}
-        onSuccess={onSuccess ? onSuccess(backUrl) : undefined}
+        onSuccess={onSuccess ? () => onSuccess(backUrl) : undefined}
       />
       {canGoBack && <BackButton to={backUrl} />}
       {record?.id > 0 && <DeleteWithConfirmButton redirect={backUrl} />}
