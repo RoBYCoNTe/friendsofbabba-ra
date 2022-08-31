3818e8f1d4db57fd4a78c6efd21e478ef3132998
RoBYCoNTe <roberto.conterosito@gmail.com>
2022-08-31 09:31:58 +0200

    fix: added support for 'helperText' in ReferenceAutocompleteInput

--- src/component/input/ReferenceAutocompleteInput.js
+++ src/component/input/ReferenceAutocompleteInput.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { ReferenceInput, AutocompleteInput } from "react-admin";
-const ReferenceAutocompleteInput = ({ optionText, ...props }) => (
+const ReferenceAutocompleteInput = ({ optionText, helperText, ...props }) => (
   <ReferenceInput {...props}>
-    <AutocompleteInput optionText={optionText} />
+    <AutocompleteInput optionText={optionText} helperText={helperText} />
   </ReferenceInput>
 );
 export default ReferenceAutocompleteInput;
