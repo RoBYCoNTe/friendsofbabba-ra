00826d38f152a37522d128371c5885e65745739c
RoBYCoNTe <roberto.conterosito@gmail.com>
2023-02-21 13:18:44 +0100

    fix: multiple files must be displayed as blocks

--- src/component/field/MediaField.js
+++ src/component/field/MediaField.js
@@ -1,10 +1,18 @@
 import { FileField } from "ra-ui-materialui";
 import React from "react";
 import { get } from "lodash";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "block",
+    clear: "both",
+  },
+}));
 const MediaField = ({ ...props }) => {
+  const classes = useStyles();
   const id = get(props, "record.id", 0);
   const source = id > 0 ? "file.path" : "filepath";
-  return <FileField {...props} source={source} />;
+  return <FileField {...props} source={source} className={classes.root} />;
 };
 export default MediaField;
