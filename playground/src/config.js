632d37f6998780578550f2cb13084bbe1cd5a5e6
wel0z <emanuele@lamacchia.dev>
2023-04-17 09:15:52 +0200

    dev: Update logo

--- playground/src/config.js
+++ playground/src/config.js
@@ -1,14 +1,13 @@
-import logo from './assets/img/logo.png';
+import logo from "./assets/img/logo.png";
 
 let environment = "PRODUCTION";
 let appUrl = `//${document.location.host}/`;
 if (appUrl.endsWith(":3000/")) {
-	appUrl = "https://crm.incisor.it.ddev.site/";
+	appUrl = "https://friendsofbabba-core-sample.ddev.site/";
 	environment = "DEVELOPER";
 }
 export const APP_URL = appUrl;
 export const API_URL = `${APP_URL}api`;
-export const DRAWER_WIDTH = 270;
 export const ENVIRONMENT = environment;
 export const RECAPTCHA_SITE_KEY = "?";
 
@@ -23,9 +22,7 @@ export const FILE_FIELDS = [
 export const fobConfig = {
 	logo,
 	logoSx: {
-		width: 180,
+		width: 80,
 	},
 	menuGroups: ["notifications", "users", "settings"],
-	// menu: <MyMenu />,
-	// menuBottom: <MyMenuBottom />,
 };
