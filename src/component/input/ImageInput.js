bda545dad78750afa28c9c9280ef1268f6dd9ff7
wel0z <emanuele@lamacchia.dev>
2023-04-21 12:30:01 +0200

    fix: create/update different sources

--- src/component/input/ImageInput.js
+++ src/component/input/ImageInput.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { ImageInput as RaImageInput } from 'react-admin';
+import {
+  ImageInput as RaImageInput,
+  useRecordContext,
+} from 'react-admin';
 
 import CoverField from '../field/CoverField';
 
 const ImageInput = (props) => {
+	const record = useRecordContext(props);
+	const source = useMemo(
+		() => (record?.id > 0 ? "file.path" : "src"),
+		[record]
+	);
+	const title = useMemo(
+		() => (record?.id > 0 ? "file.name" : "title"),
+		[record]
+	);
 	return (
 		<RaImageInput {...props}>
-			<CoverField source="src" title="title" />
+			<CoverField source={source} title={title} />
 		</RaImageInput>
 	);
 };
