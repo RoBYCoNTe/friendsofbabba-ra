3ab7c06e53332e521ec3e23db156e82998b737f6
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2023-04-22 17:36:24 +0200

    fix: added default progress value to avoid issues when changing screen

--- src/component/field/ProgressField.js
+++ src/component/field/ProgressField.js
@@ -1,12 +1,8 @@
-import React from 'react';
+import { LinearProgress, Typography } from '@mui/material';
 
+import React from 'react';
 import { get } from 'lodash';
 import { useRecordContext } from 'react-admin';
-
-import {
-  LinearProgress,
-  Typography,
-} from '@mui/material';
 
 const ProgressField = ({ source, ...props }) => {
 	const record = useRecordContext(props);
@@ -17,17 +13,17 @@ const ProgressField = ({ source, ...props }) => {
 					height: 10,
 					width: 100,
 					borderRadius: 0.5,
-					mb: 1,
+					mb: 1
 				})}
 				variant="determinate"
-				color={get(record, source) >= 100 ? "primary" : "secondary"}
-				value={get(record, source)}
+				color={get(record, source) >= 100 ? 'primary' : 'secondary'}
+				value={get(record, source, 0)}
 			/>
 			<Typography
 				variant="caption"
-				sx={(theme) => ({
-					display: "block",
-					textAlign: "center",
+				sx={() => ({
+					display: 'block',
+					textAlign: 'center'
 				})}
 			>
 				{get(record, source, 0).toFixed(2)}%
