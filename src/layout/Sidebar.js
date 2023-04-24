1b32beb2442bfeef86972fc8bbc0f7799d6e6be0
wel0z <emanuele@lamacchia.dev>
2023-04-24 10:45:09 +0200

    fix: not existing roles when not logged in

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,25 +1,38 @@
-import React, { useEffect, useMemo } from "react";
+import React, {
+  useEffect,
+  useMemo,
+} from 'react';
 
-import PropTypes from "prop-types";
-import { useGetIdentity } from "react-admin";
-import { useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { useGetIdentity } from 'react-admin';
+import { useLocation } from 'react-router-dom';
 
-import { Avatar, Box, Drawer, Link, Typography } from "@mui/material";
-import { alpha, styled, useTheme } from "@mui/material/styles";
+import {
+  Avatar,
+  Box,
+  Drawer,
+  Link,
+  Typography,
+} from '@mui/material';
+import {
+  alpha,
+  styled,
+  useTheme,
+} from '@mui/material/styles';
 
-import { NAV } from "./config";
-import useResponsive from "./hooks/useResponsive";
-import DefaultLogo from "./Logo";
-import DefaultMenu from "./menu/vertical/MenuVertical";
-import DefaultMenuBottom from "./menu/vertical/MenuVerticalBottom";
-import SidebarToggleButton from "./SidebarToggleButton";
+import { NAV } from './config';
+import useResponsive from './hooks/useResponsive';
+import DefaultLogo from './Logo';
+import DefaultMenu from './menu/vertical/MenuVertical';
+import DefaultMenuBottom from './menu/vertical/MenuVerticalBottom';
+import SidebarToggleButton from './SidebarToggleButton';
 
-const StyledAccount = styled("div")(({ theme }) => ({
-	display: "flex",
-	alignItems: "center",
+const StyledAccount = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
 	padding: theme.spacing(2, 2.5),
 	borderRadius: Number(theme.shape.borderRadius) * 1.5,
-	backgroundColor: alpha(theme.palette.grey[500], 0.12),
+	backgroundColor: alpha(theme.palette.grey[500], 0.12)
 }));
 
 const Sidebar = ({
@@ -28,18 +41,18 @@ const Sidebar = ({
 	menu: Menu = DefaultMenu,
 	menuBottom: MenuBottom = DefaultMenuBottom,
 	menuGroups = [],
-	logo,
+	logo
 }) => {
 	const { pathname } = useLocation();
 	const theme = useTheme();
-	const isDesktop = useResponsive("up", "lg");
+	const isDesktop = useResponsive('up', 'lg');
 	const { data: identity, isLoading } = useGetIdentity();
 	const Logo = useMemo(() => {
 		const logoType = typeof logo;
 		switch (logoType) {
-			case "string":
+			case 'string':
 				return <DefaultLogo src={logo} />;
-			case "function":
+			case 'function':
 				const CustomLogo = logo;
 				return <CustomLogo />;
 			default:
@@ -61,7 +74,7 @@ const Sidebar = ({
 	const renderContent = (
 		<>
 			{Logo && (
-				<Box sx={{ px: 2.5, py: 3, display: "inline-flex" }}>
+				<Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
 					{React.cloneElement(Logo)}
 				</Box>
 			)}
@@ -72,20 +85,22 @@ const Sidebar = ({
 							<Avatar src={identity.avatar} alt="Avatar" />
 						) : (
 							<Avatar sx={{ bgcolor: theme.palette.primary.main }}>
-								{identity?.name?.[0] || "".toUpperCase()}
+								{identity?.name?.[0] || ''.toUpperCase()}
 							</Avatar>
 						)}
 						<Box sx={{ ml: 2 }}>
-							<Typography variant="subtitle2" sx={{ color: "text.primary" }}>
+							<Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
 								{identity?.full_name}
 							</Typography>
-							<Typography variant="body2" sx={{ color: "text.secondary" }}>
-								{identity?.roles
-									.reduce((acc, role) => {
-										return `${acc} ${role.name}, `;
-									}, "")
-									.slice(0, -2)}
-							</Typography>
+							{identity?.roles && (
+								<Typography variant="body2" sx={{ color: 'text.secondary' }}>
+									{identity?.roles
+										.reduce((acc, role) => {
+											return `${acc} ${role.name}, `;
+										}, '')
+										.slice(0, -2)}
+								</Typography>
+							)}
 						</Box>
 					</StyledAccount>
 				</Link>
@@ -105,7 +120,7 @@ const Sidebar = ({
 			component="nav"
 			sx={{
 				flexShrink: { lg: 0 },
-				width: { lg: NAV.W_DASHBOARD },
+				width: { lg: NAV.W_DASHBOARD }
 			}}
 		>
 			<SidebarToggleButton />
@@ -116,9 +131,9 @@ const Sidebar = ({
 					PaperProps={{
 						sx: {
 							width: NAV.W_DASHBOARD,
-							bgcolor: "background.default",
-							borderRightStyle: "dashed",
-						},
+							bgcolor: 'background.default',
+							borderRightStyle: 'dashed'
+						}
 					}}
 				>
 					{renderContent}
@@ -128,10 +143,10 @@ const Sidebar = ({
 					open={openNav}
 					onClose={onCloseNav}
 					ModalProps={{
-						keepMounted: true,
+						keepMounted: true
 					}}
 					PaperProps={{
-						sx: { width: NAV.W_DASHBOARD },
+						sx: { width: NAV.W_DASHBOARD }
 					}}
 				>
 					{renderContent}
@@ -147,7 +162,7 @@ Sidebar.propTypes = {
 	menu: PropTypes.func,
 	menuBottom: PropTypes.func,
 	menuGroups: PropTypes.array,
-	logo: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+	logo: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
 };
 
 export default Sidebar;
