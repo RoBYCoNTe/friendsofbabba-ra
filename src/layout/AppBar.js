8ab2b6d371e814ecb232d9eddc700d11e46cf146
wel0z <emanuele@lamacchia.dev>
2023-04-28 15:37:29 +0200

    fix: Horizontal layout logo minWidth

--- src/layout/AppBar.js
+++ src/layout/AppBar.js
@@ -1,47 +1,56 @@
-import React, { useMemo } from "react";
+import React, { useMemo } from 'react';
 
-import PropTypes from "prop-types";
-import { LoadingIndicator, useGetIdentity } from "react-admin";
+import PropTypes from 'prop-types';
+import { LoadingIndicator, useGetIdentity } from 'react-admin';
 
-import { AppBar as MuiAppBar, IconButton, Stack, Toolbar } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
+import { AppBar as MuiAppBar, IconButton, Stack, Toolbar } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-import { useFobContext } from "../context";
-import { HEADER, NAV } from "./config";
-import useOffSetTop from "./hooks/useOffSetTop";
-import useResponsive from "./hooks/useResponsive";
-import Iconify from "./Iconify";
-import DefaultLogo from "./Logo";
-import DefaultNotificationsMenu from "./NotificationsMenu";
-import DefaultUserMenu from "./UserMenu";
-import { bgBlur } from "./utils/cssStyles";
+import { useFobContext } from '../context';
+import { HEADER, NAV } from './config';
+import useOffSetTop from './hooks/useOffSetTop';
+import useResponsive from './hooks/useResponsive';
+import Iconify from './Iconify';
+import DefaultLogo from './Logo';
+import DefaultNotificationsMenu from './NotificationsMenu';
+import DefaultUserMenu from './UserMenu';
+import { bgBlur } from './utils/cssStyles';
 
 const AppBar = ({
 	onOpenNav,
 	logo,
 	userMenu: UserMenu = DefaultUserMenu,
 	notificationsMenu: NotificationsMenu = DefaultNotificationsMenu,
-	children,
+	children
 }) => {
 	const theme = useTheme();
 	const { themeLayout } = useFobContext();
 	const { data: identity, isLoading } = useGetIdentity();
-	const isNavHorizontal = themeLayout === "horizontal";
-	const isNavMini = themeLayout === "mini";
-	const isDesktop = useResponsive("up", "lg");
+	const isNavHorizontal = themeLayout === 'horizontal';
+	const isNavMini = themeLayout === 'mini';
+	const isDesktop = useResponsive('up', 'lg');
 	const isOffset = useOffSetTop(HEADER.H_DASHBOARD_DESKTOP) && !isNavHorizontal;
 	const Logo = useMemo(() => {
 		const logoType = typeof logo;
 		switch (logoType) {
-			case "string":
-				return <DefaultLogo src={logo} sx={{ mr: 2.5, maxWidth: 80 }} />;
-			case "function":
+			case 'string':
+				return (
+					<DefaultLogo
+						src={logo}
+						sx={{ mr: 2.5, maxWidth: !isNavHorizontal ? 80 : 'none' }}
+					/>
+				);
+			case 'function':
 				const CustomLogo = logo;
-				return <CustomLogo sx={{ mr: 2.5, maxWidth: 80 }} />;
+				return (
+					<CustomLogo
+						sx={{ mr: 2.5, maxWidth: !isNavHorizontal ? 80 : 'none' }}
+					/>
+				);
 			default:
 				return undefined;
 		}
-	}, [logo]);
+	}, [logo, isNavHorizontal]);
 
 	if (isLoading) return null;
 
@@ -49,7 +58,7 @@ const AppBar = ({
 		<>
 			{isDesktop && isNavHorizontal && Logo && React.cloneElement(Logo)}
 			{!isDesktop && (
-				<IconButton onClick={onOpenNav} sx={{ mr: 1, color: "text.primary" }}>
+				<IconButton onClick={onOpenNav} sx={{ mr: 1, color: 'text.primary' }}>
 					<Iconify icon="eva:menu-2-fill" />
 				</IconButton>
 			)}
@@ -63,7 +72,7 @@ const AppBar = ({
 				{children}
 				<LoadingIndicator
 					sx={{
-						color: "text.secondary",
+						color: 'text.secondary'
 					}}
 				/>
 				<NotificationsMenu identity={identity} />
@@ -75,37 +84,37 @@ const AppBar = ({
 	return (
 		<MuiAppBar
 			sx={{
-				boxShadow: "none",
+				boxShadow: 'none',
 				height: HEADER.H_MOBILE,
 				zIndex: theme.zIndex.appBar + 1,
 				...bgBlur({
-					color: theme.palette.background.default,
+					color: theme.palette.background.default
 				}),
-				transition: theme.transitions.create(["height"], {
-					duration: theme.transitions.duration.shorter,
+				transition: theme.transitions.create(['height'], {
+					duration: theme.transitions.duration.shorter
 				}),
 				...(isDesktop && {
 					width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
 					height: HEADER.H_DASHBOARD_DESKTOP,
 					...(isOffset && {
-						height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
+						height: HEADER.H_DASHBOARD_DESKTOP_OFFSET
 					}),
 					...(isNavHorizontal && {
 						width: 1,
-						bgcolor: "background.default",
+						bgcolor: 'background.default',
 						height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
-						borderBottom: `dashed 1px ${theme.palette.divider}`,
+						borderBottom: `dashed 1px ${theme.palette.divider}`
 					}),
 					...(isNavMini && {
-						width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`,
-					}),
-				}),
+						width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`
+					})
+				})
 			}}
 		>
 			<Toolbar
 				sx={{
 					height: 1,
-					px: { lg: 5 },
+					px: { lg: 5 }
 				}}
 			>
 				{renderContent}
@@ -118,7 +127,7 @@ AppBar.propTypes = {
 	onOpenNav: PropTypes.func,
 	logo: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
 	userMenu: PropTypes.func,
-	notificationsMenu: PropTypes.func,
+	notificationsMenu: PropTypes.func
 };
 
 export default AppBar;
