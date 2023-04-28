dd0624c305db1de104bdb583eb21d8a6e62debf1
wel0z <emanuele@lamacchia.dev>
2023-04-28 15:28:30 +0200

    fix: Undefined record rendering

--- src/component/crud/Title.js
+++ src/component/crud/Title.js
@@ -1,21 +1,21 @@
-import {
-  useRecordContext,
-  useTranslate,
-} from 'react-admin';
+import { useRecordContext, useTranslate } from 'react-admin';
 
 const Title = ({ content, smartCount = 1, ...props }) => {
 	const record = useRecordContext(props);
 	const translate = useTranslate();
 	const count = record?.id > 0 ? 1 : 0;
-	const contents = content?.split("||||");
+	const contents = content?.split('||||');
 	const target = count > 0 ? 1 : 0;
 	const message = contents?.[target] || content;
+	if (record === undefined) {
+		return null;
+	}
 	return content
 		? translate(message, {
 				_: message,
 				count,
 				smart_count: smartCount,
-				...record,
+				...record
 		  })
 		: null;
 };
