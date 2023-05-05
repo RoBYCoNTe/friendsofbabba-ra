3c1a837d1a050a9c63c82a8f406ad3f9f2a1cfcf
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2023-05-05 18:21:24 +0200

    fix: added support for local uploaded file or remote previewed files

--- src/component/input/ImageInput.js
+++ src/component/input/ImageInput.js
@@ -1,26 +1,49 @@
-import React, { useMemo } from 'react';
-
 import {
-  ImageInput as RaImageInput,
-  useRecordContext,
+	ImageInput as RaImageInput,
+	useEditContext,
+	useRecordContext
 } from 'react-admin';
+import React, { useEffect, useMemo } from 'react';
 
 import CoverField from '../field/CoverField';
+import PropTypes from 'prop-types';
+import { get } from 'lodash';
+import { useWatch } from 'react-hook-form';
 
-const ImageInput = (props) => {
+const ImageInput = ({ baseUrl, ...props }) => {
+	const file = useWatch({ name: props.source });
 	const record = useRecordContext(props);
 	const source = useMemo(
-		() => (record?.id > 0 ? "file.path" : "src"),
-		[record]
+		() => (record?.id && !file?.rawFile ? 'file.path' : 'src'),
+		[record?.id, file?.rawFile]
 	);
 	const title = useMemo(
-		() => (record?.id > 0 ? "file.name" : "title"),
-		[record]
+		() => (record?.id && !file?.rawFile ? 'file.name' : 'title'),
+		[record?.id, file?.rawFile]
+	);
+	const _record = useMemo(
+		() => ({
+			...record,
+			file: {
+				path: `${baseUrl}/${get(record, props.source)}`,
+				name: get(record, props.source)
+			}
+		}),
+		[record, baseUrl, props.source]
 	);
 	return (
-		<RaImageInput {...props}>
-			<CoverField source={source} title={title} />
+		<RaImageInput {...props} record={_record} accept="image/*">
+			<CoverField
+				record={{ ..._record, ...file }}
+				source={source}
+				title={title}
+			/>
 		</RaImageInput>
 	);
 };
+
+ImageInput.propTypes = {
+	baseUrl: PropTypes.string
+};
+
 export default ImageInput;
