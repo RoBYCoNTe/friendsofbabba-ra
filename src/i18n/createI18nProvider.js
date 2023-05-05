95cbdef9c0584019c51c03173bf554db52f65e10
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2023-05-05 18:22:12 +0200

    dev: added support for allowMissing to avoid intl warning during development

--- src/i18n/createI18nProvider.js
+++ src/i18n/createI18nProvider.js
@@ -1,13 +1,23 @@
-import { get } from "lodash";
-import polyglotI18nProvider from "ra-i18n-polyglot";
-import { resolveBrowserLocale } from "react-admin";
+import { get } from 'lodash';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import { resolveBrowserLocale } from 'react-admin';
 
-const createI18nProvider = ({ locale = "en", languages }) =>
-	polyglotI18nProvider(() => {
-		if (process.env.NODE_ENV !== "production") {
-			localStorage.setItem("locale", locale);
+const createI18nProvider = ({
+	locale = 'en',
+	languages,
+	allowMissing = false
+}) =>
+	polyglotI18nProvider(
+		() => {
+			if (process.env.NODE_ENV !== 'production') {
+				localStorage.setItem('locale', locale);
+			}
+			return get(languages, locale, {});
+		},
+		resolveBrowserLocale(),
+		{
+			allowMissing
 		}
-		return get(languages, locale, {});
-	}, resolveBrowserLocale());
+	);
 
 export default createI18nProvider;
