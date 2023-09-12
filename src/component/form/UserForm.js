645e638c0edece71a82a5c560d648ec2bf175468
wel0z <emanuele@lamacchia.dev>
2023-09-12 11:16:33 +0200

    Fix: Avatar cause wrong local storage items

--- src/component/form/UserForm.js
+++ src/component/form/UserForm.js
@@ -14,19 +14,23 @@ const UserForm = ({ children, onSubmit, ...props }) => {
 	const { refetch } = useGetIdentity();
 	const resource = useResourceContext();
 	const redirect = useRedirect();
+	const { identity } = useGetIdentity();
+	console.log(identity);
 	const save = useSaveMutation({
 		...props,
 		refresh: undefined,
 		redirect: 'list',
 		onSuccess: async (response) => {
-			localStorage.setItem('avatar', get(response, 'avatar.file.path'));
-			localStorage.setItem('email', response.email);
-			localStorage.setItem('roles', JSON.stringify(response.roles));
-			localStorage.setItem('profile', JSON.stringify(response.profile));
+			if (identity?.id === response?.id) {
+				localStorage.setItem('avatar', get(response, 'avatar.file.path'));
+				localStorage.setItem('email', response.email);
+				localStorage.setItem('roles', JSON.stringify(response.roles));
+				localStorage.setItem('profile', JSON.stringify(response.profile));
+				setTimeout(() => {
+					refetch();
+				}, 500);
+			}
 			redirect('list', resource);
-			setTimeout(() => {
-				refetch();
-			}, 500);
 		}
 	});
 
