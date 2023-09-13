9cbc20302512e955a9d4f96571ad94ebbc6d2418
wel0z <emanuele@lamacchia.dev>
2023-09-13 10:05:30 +0200

    Fix: null avatar image

--- src/layout/UserMenu.js
+++ src/layout/UserMenu.js
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
-
-import { useLogout, useTranslate } from "react-admin";
+import React, { useState } from 'react';
 
 import {
-	Avatar,
-	Box,
-	Divider,
-	IconButton,
-	MenuItem,
-	Typography,
-} from "@mui/material";
+  useLogout,
+  useTranslate,
+} from 'react-admin';
+
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  MenuItem,
+  Typography,
+} from '@mui/material';
 // @mui
-import { alpha, useTheme } from "@mui/material/styles";
+import {
+  alpha,
+  useTheme,
+} from '@mui/material/styles';
 
 import {
-	useIsImpersonating,
-	useUndoImpersonate,
-} from "../data/createAuthProvider";
-import { MenuPopover } from "./menu-popover";
+  useIsImpersonating,
+  useUndoImpersonate,
+} from '../data/createAuthProvider';
+import { MenuPopover } from './menu-popover';
 
 const UserMenu = ({ identity, children }) => {
 	const theme = useTheme();
@@ -43,23 +49,23 @@ const UserMenu = ({ identity, children }) => {
 				sx={{
 					p: 0,
 					...(open && {
-						"&:before": {
+						'&:before': {
 							zIndex: 1,
 							content: "''",
-							width: "100%",
-							height: "100%",
-							borderRadius: "50%",
-							position: "absolute",
-							bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
-						},
-					}),
+							width: '100%',
+							height: '100%',
+							borderRadius: '50%',
+							position: 'absolute',
+							bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8)
+						}
+					})
 				}}
 			>
-				{identity?.avatar ? (
-					<Avatar src={identity.avatar} alt="Avatar" />
+				{identity?.avatar && identity?.avatar !== 'null' ? (
+					<Avatar src={identity.avatar} alt={identity?.name} />
 				) : (
 					<Avatar sx={{ bgcolor: theme.palette.primary.main }}>
-						{identity?.name?.[0] || "".toUpperCase()}
+						{identity?.name?.[0] || 'A'.toUpperCase()}
 					</Avatar>
 				)}
 			</IconButton>
@@ -68,16 +74,16 @@ const UserMenu = ({ identity, children }) => {
 					<Typography variant="subtitle2" noWrap>
 						{identity.name}
 					</Typography>
-					<Typography variant="body2" sx={{ color: "text.secondary" }} noWrap>
+					<Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
 						{identity.email}
 					</Typography>
 				</Box>
 				{React.Children.map(children, (child) =>
 					React.cloneElement(child, {
-						onClick: handleClose,
+						onClick: handleClose
 					})
 				)}
-				<Divider sx={{ borderStyle: "dashed" }} />
+				<Divider sx={{ borderStyle: 'dashed' }} />
 				{isImpersonating && (
 					<>
 						<MenuItem
@@ -87,9 +93,9 @@ const UserMenu = ({ identity, children }) => {
 							}}
 							sx={{ m: 1 }}
 						>
-							{translate("ra.auth.impersonating.undo", identity)}
+							{translate('ra.auth.impersonating.undo', identity)}
 						</MenuItem>
-						<Divider sx={{ borderStyle: "dashed" }} />
+						<Divider sx={{ borderStyle: 'dashed' }} />
 					</>
 				)}
 				<MenuItem
@@ -98,7 +104,7 @@ const UserMenu = ({ identity, children }) => {
 					}}
 					sx={{ m: 1 }}
 				>
-					{translate("ra.auth.logout")}
+					{translate('ra.auth.logout')}
 				</MenuItem>
 			</MenuPopover>
 		</>
